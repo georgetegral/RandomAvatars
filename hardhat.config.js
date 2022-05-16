3250c105e034b290ec0cdc7f47ce158353dcc7f1
georgetegral <jorgerenemg@icloud.com>
2022-05-16 11:21:13 -0500

    Added dependencies and .env with network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
+
+const projectId = process.env.INFURA_PROJECT_ID
+const privateKey = process.env.DEPLOYER_SIGNER_PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/"+projectId,
+      accounts: [privateKey]
+    }
+  }
 };
